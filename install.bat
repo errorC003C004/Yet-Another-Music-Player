98ebb6ea6d7bf3ba694129c81aa6473078bc1640
errorC003C004 <errorC003C004@mail.com>
2026-05-19 22:21:18 -0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,9 +1,26 @@
 @echo off
 color 2
-
+goto full
+:full
 pyinstaller !musicplayer.py ^
   --collect-data pykakasi ^
   --collect-all argostranslate ^
+  --hidden-import scipy._cyutility ^
+  --collect-submodules scipy ^
   --noconfirm ^
   --windowed ^
   --onefile
+exit
+
+:part
+pyinstaller !musicplayer.py ^
+  --collect-data pykakasi ^
+  --hidden-import scipy._cyutility ^
+  --exclude-module argostranslate ^
+  --exclude-module torch ^
+  --exclude-module torchvision ^
+  --exclude-module torchaudio ^
+  --noconfirm ^
+  --windowed ^
+  --onefile
+exit
